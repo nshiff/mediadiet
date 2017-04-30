fee0ddd0f8cb3e86369747906f8d7ecbce9261a5
nate <nshiff@users.noreply.github.com>
2017-04-30 17:40:47 -0400

    echoing input on submit

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,26 +5,32 @@
 
 
 
+var Reporter = {
+    NEWLINE: "\n",
+    generateReport: function(score_economic, score_social){
+
+
+        var report = "";
+        report += "<pre>";
+
+        report += "score_economic: " + score_economic;
+        report += this.NEWLINE;
+        report += "score_social: " + score_social;
+
+
+        report += "</pre>";
+        return report;
+    }
 
 
 
+};
 
 jQuery(document).ready(function(){
-    console.log('ready for action');
-
-
-
-
-
     jQuery('form#beliefs a').on('click',function(){
-        alert('placeholder for science');
+        var score_economic = jQuery('input#economic').val();
+        var score_social = jQuery('input#social').val();
+        var report = Reporter.generateReport(score_economic, score_social);
+        jQuery('#output').html(report);
     });
-
-
-
-
-
-
 });
-
-
