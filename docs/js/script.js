e05c7811c2f94de2ecd683711752fd49646127f9
nate <nshiff@users.noreply.github.com>
2017-04-30 17:08:58 -0400

    strawman submit button

---
+++ docs/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+jQuery(document).ready(function(){
+    console.log('ready for action');
+
+
+
+
+
+    jQuery('form#beliefs').on('click',function(){
+        alert('placeholder for science');
+    });
+
+
+
+
+
+
+});
+
+
